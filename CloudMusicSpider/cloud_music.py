ea2da1208dc205d353cbdf884ca3ed0c71708c84
ChienHo <467360687@qq.com>
2018-10-19 23:22:04 +0800

    feature: 增加eapi获取用户歌单内容接口

--- CloudMusicSpider/cloud_music.py
+++ CloudMusicSpider/cloud_music.py
@@ -11,7 +11,12 @@ import json
 class CloudMusicApi:
 
     __AES_KEY = b'e82ckenh8dichen8'
-
+    """
+    网易云音乐eapi调用
+    
+    :param num_pools: 线程池数量
+    :param proxy: 若使用代理则传代理地址
+    """
     def __init__(self, num_pools=10, proxy=None):
         headers = {
             'Accept': '*/*',
@@ -27,16 +32,54 @@ class CloudMusicApi:
         else:
             self.__http_pool = urllib3.ProxyManager(proxy, num_pools, headers)
 
+    """
+    获取用户播放列表
+    
+    :param uid: 用户ID
+    :param offset: 偏移量
+    :param limit: 每次获取的数量
+    """
     def user_play_list(self, uid, offset=0, limit=1000):
         params = {
+            'all': 'true',
             'uid': str(uid),
             'offset': str(offset),
             'limit': str(limit),
         }
         return self._request_eapi('/eapi/user/playlist', b'/api/user/playlist/', params)
 
+    """
+    获取歌单详情
+    
+    :param play_list_id: 播放列表ID
+    :track_id_len: 获取歌曲id的数量
+    :track_name_len: 获取具体歌曲名字的数量
+    """
+    def play_list_detail(self, play_list_id, track_id_len=-1, track_name_len=200):
+        params = {
+            'id': str(play_list_id),
+            'c': '[]',
+            't': str(track_id_len),
+            'n': str(track_name_len),
+            's': '0',
+        }
+        data = self._request_eapi('/eapi/v3/playlist/detail', b'/api/v3/playlist/detail', params)
+        if data['code'] == 200:
+            return data['playlist']
+        else:
+            return None
+
+    """
+    获取歌曲详情
+    :param song_ids: 歌曲的id，需要传list({'id': xxx, 'v': 0})
+    """
+    def song_detail(self, song_ids):
+        params = {
+            'c': json.dumps(song_ids, separators=(',', ':')),
+        }
+        return self._request_eapi('/eapi/v3/song/detail/', b'/api/v3/song/detail/', params)
+
     def _request_eapi(self, gateway_path, request_path, params):
-        params['all'] = 'true'
         params['verifyId'] = 1
         params['os'] = 'OSX'
         params['header'] = json.dumps({
--- CloudMusicSpider/run.py
+++ CloudMusicSpider/run.py
@@ -4,3 +4,9 @@ from cloud_music import CloudMusicApi
 if __name__ == '__main__':
     api = CloudMusicApi()
     print(api.user_play_list(10001))
+    play_list = api.play_list_detail(409947357, -1, 200)
+    print(play_list['tracks'])
+    song_ids = []
+    for item in play_list['trackIds']:
+        song_ids.append({'id': item['id'], 'v': 0})
+    print(api.song_detail(song_ids[200:700]))
